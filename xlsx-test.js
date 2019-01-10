05a9c654829494b09e42f9e7269278971506c35a
Josh Beard <josh@jlbeard.com>
2019-01-10 15:51:46 -0500

    Updated included lib and test

--- xlsx-test.js
+++ xlsx-test.js
@@ -6,13 +6,51 @@ const config = {
                 {
                     value: 'ABC',
                     type: 'string',
-                    style: 'bgColor="00FCF305"'
+                    style: 'bgColor=\"00FCF305\"'
                 },
                 {
                     value: 'DEF',
                     type: 'string'
-                }
-            ]
+                },               
+                {
+                    value: 'GHI',
+                    type: 'string',
+                    style: 'bgColor=\"00FCF305\"'
+                },
+                {
+                    value: 'JKL',
+                    type: 'string'
+                }, 
+                {
+                    value: 'MNO',
+                    type: 'string',
+                    style: 'bgColor=\"00FCF305\"'
+                }  
+            ],
+            [
+                {
+                    value: 'PQR',
+                    type: 'string'
+                },
+                {
+                    value: 'STU',
+                    type: 'string',
+                    style: 'bgColor=\"00FCF305\"'
+                },               
+                {
+                    value: 'VWX',
+                    type: 'string',
+                },
+                {
+                    value: 'YZA',
+                    type: 'string',
+                    style: 'bgColor=\"00FCF305\"'
+                }, 
+                {
+                    value: 'BCD',
+                    type: 'string',
+                }  
+            ],
         ]
     }
 }
